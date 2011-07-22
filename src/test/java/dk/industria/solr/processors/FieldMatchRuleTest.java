abf1cfc3d7b10e770058f28895c550883d9c6f3a
James Lindstorff <jlindstorff@gmail.com>
2011-07-22 21:34:58 +0200

    Removed IDE boilerplate header

--- src/test/java/dk/industria/solr/processors/FieldMatchRuleTest.java
+++ src/test/java/dk/industria/solr/processors/FieldMatchRuleTest.java
@@ -3,13 +3,6 @@ package dk.industria.solr.processors;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- * Created by IntelliJ IDEA.
- * User: james
- * Date: 7/22/11
- * Time: 8:29 PM
- * To change this template use File | Settings | File Templates.
- */
 public class FieldMatchRuleTest {
 
 
