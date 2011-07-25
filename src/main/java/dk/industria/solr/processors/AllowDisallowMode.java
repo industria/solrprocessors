1ab6ade97a33514bfa18e797b1ef47a570ab9cb5
James Lindstorff <jlindstorff@gmail.com>
2011-07-25 13:08:45 +0200

    Enum for representing the mode of operation

---
+++ src/main/java/dk/industria/solr/processors/AllowDisallowMode.java
@@ -0,0 +1,11 @@
+package dk.industria.solr.processors;
+
+/**
+ * Indicates the mode used by the AllowDisallowIndexerProcessor
+ */
+public enum AllowDisallowMode {
+    UNKNOWN,
+    ALLOW,
+    DISALLOW;
+}
+
