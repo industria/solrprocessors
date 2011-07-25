0d304d091077c4a4993a0d748d42a368a98100ff
James Lindstorff <jlindstorff@gmail.com>
2011-07-25 09:12:23 +0200

    Implemented safe field extraction for HTML strip char filter processor and also added unit test for that

--- src/main/java/dk/industria/solr/processors/HTMLStripCharFilterProcessorFactory.java
+++ src/main/java/dk/industria/solr/processors/HTMLStripCharFilterProcessorFactory.java
@@ -1,6 +1,7 @@
 package dk.industria.solr.processors;
 
-
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -29,19 +30,82 @@ public class HTMLStripCharFilterProcessorFactory extends UpdateRequestProcessorF
 
 
     /**
+     * Generate a string containing the fields configured, the string is
+     * on the form {field1} {field2} ... {fieldn}
+     * @param fields The fields for the field string.
+     * @return String on the form {field1} {field2} ... {fieldn}
+     */
+    private static String configuredFieldsString(List<String> fields) {
+        StringBuilder sb = new StringBuilder();
+        for(String field : fields) {
+            sb.append(" {");
+            sb.append(field);
+            sb.append("}");
+        }
+        return sb.toString();
+    }
+
+
+    /**
+     * Extract field names from init arguments.
+     * That is fields with a key of field and a type of String.
+     * @param initArguments NamedList containing the init arguments.
+     * @return List of field names.
+     */
+    private static List<String> extractFields(final NamedList initArguments) {
+	List<String> fieldNames = new ArrayList<String>();
+	List valuesWithField = initArguments.getAll("field");
+	for(Object value : valuesWithField) {
+	    if(value instanceof String) {
+		String valueToAdd = ((String)value).trim();
+		if(0 < valueToAdd.length()) {
+		    if(logger.isDebugEnabled()) {
+			logger.debug("Adding field: [" + valueToAdd + "]");
+		    }
+		    fieldNames.add(valueToAdd);
+		} else {
+		    logger.warn("Field of length 0 not added value was [" + value.toString() +"]");
+		}
+	    } else {
+		if(null == value) {
+		    logger.warn("Misconfigured field, value was [null]");
+		} else {
+		    logger.warn("Misconfigured field of type [" + value.getClass().getName() + "] with value [" + value.toString() +"]");
+		}
+	    }
+	}
+	return fieldNames;
+    }
+
+
+    /**
+     * Get the list of field names configured for processing.
+     * @return Unmodifiable list of field names configured.
+     */
+    public List<String> getFields() {
+	if (null == fieldsToProcess) {
+	    return Collections.EMPTY_LIST;
+	}
+	return Collections.unmodifiableList(fieldsToProcess);
+    }
+
+
+
+    /**
      * Init called by SOLR processor chain
      * The values configured for keys field is extracted to fieldsToProcess.
      * @param args NamedList of parameters set in the processor definition (solrconfig.xml)
      */
     public void init(final NamedList args) {
-        // TODO: Really can do that unless solrconfig is always str elements otherwise types break
-        this.fieldsToProcess = args.getAll("field");
+        this.fieldsToProcess = extractFields(args);
+
+	if(logger.isDebugEnabled()) {
+            String fls = configuredFieldsString(this.fieldsToProcess);
+            logger.debug("Configured with fields [" + fls + "]");
+        }
 
         if(this.fieldsToProcess.isEmpty()) {
-            logger.warn("No fields defined for HTMLStripCharFilterProcessor");
-        } else if(logger.isDebugEnabled()) {
-            String fls = configuredFieldsString(this.fieldsToProcess);
-            logger.debug("HTMLStripCharFilterProcessor fields:" + fls);
+            logger.warn("No fields configured");
         }
     }
 
@@ -62,21 +126,5 @@ public class HTMLStripCharFilterProcessorFactory extends UpdateRequestProcessorF
     }
 
 
-
-    /**
-     * Generate a string containing the fields configured, the string is
-     * on the form {field1} {field2} ... {fieldn}
-     * @param fields The fields for the field string.
-     * @return String on the form {field1} {field2} ... {fieldn}
-     */
-    private static String configuredFieldsString(List<String> fields) {
-        StringBuilder sb = new StringBuilder();
-        for(String field : fields) {
-            sb.append(" {");
-            sb.append(field);
-            sb.append("}");
-        }
-        return sb.toString();
-    }
 
 }
--- src/test/java/dk/industria/solr/processors/HTMLStripCharFilterProcessorFactoryTest.java
+++ src/test/java/dk/industria/solr/processors/HTMLStripCharFilterProcessorFactoryTest.java
@@ -1,6 +1,10 @@
 package dk.industria.solr.processors;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.solr.update.processor.UpdateRequestProcessor;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -9,28 +13,116 @@ import org.apache.solr.common.util.NamedList;
 public class HTMLStripCharFilterProcessorFactoryTest {
 
 
+    /**
+     * Compare to lists of string 
+     * @param expected List of fields expected.
+     * @param actual List of field actually in the list.
+     * @return True if both are not null, the same size and all elements are equal.
+     */
+    private static boolean compareFieldLists(List<String> expected, List<String> actual) {
+	if((null == expected) || (null == actual)) {
+	    return false;
+	}
 
+	if(expected.size() != actual.size()) {
+	    return false;
+	}
 
-    @Test public void initWithLegal() {
-        // TODO: Implement test
-        NamedList l = new NamedList();
-        l.add("field", "header");
-        l.add("field", "content");
-        l.add("ignored", 100);
-
-        HTMLStripCharFilterProcessorFactory f = new HTMLStripCharFilterProcessorFactory();
-        f.init(l);
-        UpdateRequestProcessor p = f.getInstance(null, null, null);
-        assertTrue(true);
+	for(int i = 0; i < expected.size(); i++) {
+	    String expectedItem = expected.get(i);
+	    String actualItem = actual.get(i);
+	    if(!expectedItem.equals(actualItem)) {
+		return false;
+	    }
+	}
+	return true;
     }
 
-    @Test public void initWithIllegal() {
-        //TODO: implement test
-        assertTrue(true);
+    /** 
+     * Get list with header and content strings.
+     * @return List of strings containing header and content.
+     */
+    private static List<String> expectedHeaderContent() {
+	List<String> expectedFields = new ArrayList<String>();
+	expectedFields.add("header");
+	expectedFields.add("content");
+	return expectedFields;
     }
 
+    /**
+     * Test simple legal configuration of the factory with fields
+     */
+    @Test public void initFactoryWithLegal() {
+	NamedList input = new NamedList();
+        input.add("field", "header");
+        input.add("field", "content");
+        input.add("ignored", 100);
 
+        HTMLStripCharFilterProcessorFactory factory = new HTMLStripCharFilterProcessorFactory();
+        factory.init(input);
 
+	List<String> actualFields = factory.getFields();
+	
+	List<String> expectedFields = expectedHeaderContent();
+
+	assertTrue(compareFieldLists(actualFields, expectedFields));
+
+	//        UpdateRequestProcessor processor = factory.getInstance(null, null, null);
+    }
+
+    /**
+     * Test with illegal types in the arguments list similar to 
+     * Solr configuration with an element other than str
+     */
+    @Test public void initFactoryWithIllegalTypeFields() {
+	NamedList input = new NamedList();
+        input.add("field", "header");
+        input.add("field", Integer.valueOf(100));
+        input.add("field", "content");
+
+        HTMLStripCharFilterProcessorFactory factory = new HTMLStripCharFilterProcessorFactory();
+        factory.init(input);
+
+	List<String> actualFields = factory.getFields();
+
+	List<String> expectedFields = expectedHeaderContent();
+
+	assertTrue(compareFieldLists(actualFields, expectedFields));
+
+    }
+
+    /**
+     * Test with illegal types in the arguments list similar to 
+     * Solr configuration with an element other than str
+     */
+    @Test public void initFactoryWithAllIllegalTypeFields() {
+	NamedList input = new NamedList();
+        input.add("field", Long.valueOf(1));
+        input.add("field", Integer.valueOf(100));
+        input.add("field", new NamedList());
+	input.add("field", "");
+	input.add("field", "  ");
+	input.add("field", null);
+
+        HTMLStripCharFilterProcessorFactory factory = new HTMLStripCharFilterProcessorFactory();
+        factory.init(input);
+
+	List<String> actualFields = factory.getFields();
+
+	assertNotNull(actualFields);
+	assertEquals(0, actualFields.size());
+    }
+
+    /**
+     * Make sure calling getFields before init does break things
+     */
+    @Test public void fieldsBeforeInit() {
+        HTMLStripCharFilterProcessorFactory factory = new HTMLStripCharFilterProcessorFactory();
+	List<String> actualFields = factory.getFields();
+	assertNotNull(actualFields);
+	assertEquals(0, actualFields.size());
+	
+    }
 
 
 
