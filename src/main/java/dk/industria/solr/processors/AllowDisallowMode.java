9147edbe75772482a07086d21a561728fe57ecce
James Lindstorff <jlindstorff@gmail.com>
2011-07-26 12:54:50 +0200

    Added comments to the Enum

--- src/main/java/dk/industria/solr/processors/AllowDisallowMode.java
+++ src/main/java/dk/industria/solr/processors/AllowDisallowMode.java
@@ -1,11 +1,29 @@
 package dk.industria.solr.processors;
 
 /**
- * Indicates the mode used by the AllowDisallowIndexerProcessor
+ * Indicate the mode of operation.
+ * @see AllowDisallowIndexingProcessor
  */
 public enum AllowDisallowMode {
+    /**
+     * Indicates the mode of operation is unknown.
+     *
+     * No rules will be run and all documents will be indexed.
+     */
     UNKNOWN,
+    /**
+     * Indicates the mode of operation is allow.
+     *
+     * In allow mode, only documents that matches a rule
+     * will be indexed. Everything else will be filtered out.
+     */
     ALLOW,
+    /**
+     * Indicates the mode of operation is disallow. 
+     *
+     * In disallow mode, all documents will be indexed, 
+     * except for those that matches a rule.
+     */
     DISALLOW
 }
 
