1ae9b1defa2f5d5d72bd7083b2a3ddcac1fc7cab
James Lindstorff <jlindstorff@gmail.com>
2015-12-11 07:15:27 +0100

    Avoid null and commenting style

--- src/main/scala/dk/industria/solr/processors/FieldPatternReplaceRules.scala
+++ src/main/scala/dk/industria/solr/processors/FieldPatternReplaceRules.scala
@@ -15,46 +15,45 @@
  */
 package dk.industria.solr.processors
 
-/**
- * Implements a field with a list of pattern replace rules attached..
- * @param fieldName Name of the fields the rules should be attached to.
- */
+/** Implements a field with a list of pattern replace rules attached.
+  *
+  * @param fieldName Name of the fields the rules should be attached to.
+  */
 class FieldPatternReplaceRules(val fieldName: String) {
-  require(null != fieldName)
-  /**
-   * List of pattern replace rules attached to the field.
-   * @see PatternReplaceRule
-   */
+  require(Option(fieldName).isDefined)
+
+  /** List of pattern replace rules attached to the field.
+    * @see PatternReplaceRule
+    */
   private var _rules: List[PatternReplaceRule] = Nil
 
-  /**
-   * Get pattern replace rules attached to the field.
-   *
-   * @return List of pattern replace rules for the field.
-   */
+  /** Get pattern replace rules attached to the field.
+    *
+    * @return List of pattern replace rules for the field.
+    */
   def rules: List[PatternReplaceRule] = _rules
 
-  /**
-   * Add a pattern replace rule to the field.
-   *
-   * @param rule PatternReplaceRule to add to the field.
-   * @throws IllegalArgumentException if the rule is null.
-   */
+  /** Add a pattern replace rule to the field.
+    *
+    * @param rule PatternReplaceRule to add to the field.
+    * @throws IllegalArgumentException if the rule is null.
+    */
   def add(rule: PatternReplaceRule): Unit = {
-    require(null != rule)
+    require(Option(rule).isDefined)
+
     val ruleList = rule :: _rules
     _rules = ruleList.reverse
   }
 
-  /**
-   * Replace patterns in the value according to the rules.
-   *
-   * @param value Value to apply the pattern replace rules to.
-   * @return New value after pattern replace rules have been applied.
-   * @throws IllegalArgumentException if called with a null value.
-   */
+  /** Replace patterns in the value according to the rules.
+    *
+    * @param value Value to apply the pattern replace rules to.
+    * @return New value after pattern replace rules have been applied.
+    * @throws IllegalArgumentException if called with a null value.
+    */
   def replace(value: String): String = {
-    require(null != value)
+    require(Option(value).isDefined)
+
     (value /: _rules)((v, rule) => rule.replace(v))
   }
 }
